e713943d0ce87aa9a35c8b3b01830613fde01708
Hyrum Paulsen <hyrum@hpaulsen.com>
2013-04-26 14:20:27 -0700

    Centralized server write directories, added install check

---
+++ app/config.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hyrum
+ * Date: 4/26/13
+ * Time: 8:57 AM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ public/install.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hyrum
+ * Date: 4/25/13
+ * Time: 2:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
